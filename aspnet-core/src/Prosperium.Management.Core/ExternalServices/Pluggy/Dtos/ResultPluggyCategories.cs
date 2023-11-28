ef0153a71b784c20420340d6b2c2795c0e9488c1
Felipe Aparecido Martins <fmi433812@gmail.com>
2023-11-28 13:17:19 -0300

    Added the ResultPluggyCategories class

---
+++ aspnet-core/src/Prosperium.Management.Core/ExternalServices/Pluggy/Dtos/ResultPluggyCategories.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Prosperium.Management.ExternalServices.Pluggy.Dtos
+{
+    public class ResultPluggyCategories
+    {
+        public int Total { get; set; }
+        public int TotalPages { get; set; }
+        public int Page { get; set; }
+        public List<PluggyCategory> Results { get; set; }
+    }
+
+    public class PluggyCategory
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string DescriptionTranslated { get; set; }
+        public string ParentId { get; set; }
+        public string ParentDescription { get; set; }
+    }
+}
